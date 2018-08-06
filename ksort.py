f716f2afd2170f3eddfeed91e9703e4f8e413136
alviner <gr.eyed.black.fox@gmail.com>
2018-08-06 23:34:30 +1000

    Twenty-third task

---
+++ ksort.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+import random
+import string
+import time
+
+
+class Ksort():
+    def __init__(self, len=8):
+        self.alph = string.ascii_lowercase[0:len]
+        self.values = [{'count': 0, 'value': self._unhash(x)} for x in range(len * 100)]
+
+    def _hash(self, item: str):
+        a, m, n = item
+        a = self.alph.index(a)
+        return int(f'{a}{m}{n}')
+
+    def _unhash(self, index: int):
+        hash_value = f'{str(index):0>3}'
+        return self.alph[int(hash_value[0])] + hash_value[1:]
+
+    def append(self, item: str):
+        hash_value = self._hash(item)
+        self.values[hash_value]['count'] += 1
+
+    def sort(self):
+        res = []
+        for i, item in enumerate(self.values):
+            while item['count'] > 0:
+                item['count'] -= 1
+                res.append(item['value'])
+        return res
+
+
+def test_ksort():
+    items = []
+    alph = string.ascii_lowercase[0:8]
+
+    for i in range(10000):
+        value = f'{alph[random.randint(0, 7)]}{random.randint(0, 9)}{random.randint(0, 9)}'
+        items.append(value)
+    ksort = Ksort()
+    for item in items:
+        ksort.append(item)
+
+    start = time.time()
+    ksort_items = ksort.sort()
+    print(f'Ksort time {time.time() - start}')
+
+    start = time.time()
+    items.sort()
+    print(f'Sort time {time.time() - start}')
+
+    assert ksort_items == items
+
+
+if __name__ == '__main__':
+    test_ksort()
