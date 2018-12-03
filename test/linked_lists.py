d8bd005f2e65b33d2c3592ec3d1a7b09ebd31f1b
alviner <gr.eyed.black.fox@gmail.com>
2018-12-03 22:42:15 +1000

    remove tests from linked lists

---
+++ test/linked_lists.py
@@ -0,0 +1,264 @@
+# -*- coding: utf-8 -*-
+
+from single_linked_list import LinkedList, Node, merge_list
+
+def remove_test():
+    s_list = LinkedList()
+    s_list.add_in_tail(Node(1))
+    s_list.add_in_tail(Node(2))
+    s_list.add_in_tail(Node(1))
+    s_list.add_in_tail(Node(2))
+    s_list.add_in_tail(Node(1))
+    s_list.add_in_tail(Node(10))
+    s_list.add_in_tail(Node(3))
+    s_list.add_in_tail(Node(1))
+    s_list.add_in_tail(Node(4))
+    s_list.add_in_tail(Node(1))
+
+    s_list_test = LinkedList()
+    s_list_test.add_in_tail(Node(2))
+    s_list_test.add_in_tail(Node(1))
+    s_list_test.add_in_tail(Node(2))
+    s_list_test.add_in_tail(Node(1))
+    s_list_test.add_in_tail(Node(1))
+    s_list_test.add_in_tail(Node(1))
+
+    s_list.delete(1)
+    s_list.delete(10)
+    s_list.delete(3)
+    s_list.delete(4)
+    assert s_list.len() == s_list_test.len()
+
+    node = s_list.head
+    node_test = s_list_test.head
+    while node is not None and node_test is not None:
+        assert node.value == node_test.value
+        node = node.next
+        node_test = node_test.next
+
+
+def mass_remove_test():
+    s_list = LinkedList()
+    s_list.add_in_tail(Node(1))
+    s_list.add_in_tail(Node(1))
+    s_list.add_in_tail(Node(1))
+    s_list.add_in_tail(Node(2))
+    s_list.add_in_tail(Node(2))
+    s_list.add_in_tail(Node(128))
+    s_list.add_in_tail(Node(128))
+    s_list.add_in_tail(Node(9))
+    s_list.add_in_tail(Node(9))
+    s_list.add_in_tail(Node(1))
+    s_list.add_in_tail(Node(9))
+    s_list.add_in_tail(Node(1))
+    s_list.add_in_tail(Node(9))
+
+    s_list_test = LinkedList()
+    s_list_test.add_in_tail(Node(2))
+    s_list_test.add_in_tail(Node(2))
+    s_list_test.add_in_tail(Node(128))
+    s_list_test.add_in_tail(Node(128))
+
+    s_list.delete(1, all=True)
+    s_list.delete(9, all=True)
+    assert s_list.len() == s_list_test.len()
+
+    node = s_list.head
+    node_test = s_list_test.head
+    while node is not None and node_test is not None:
+        assert node.value == node_test.value
+        node = node.next
+        node_test = node_test.next
+
+
+def clear_test():
+    s_list = LinkedList()
+    s_list.add_in_tail(Node(1))
+    s_list.add_in_tail(Node(1))
+    s_list.add_in_tail(Node(1))
+    s_list.add_in_tail(Node(2))
+    s_list.add_in_tail(Node(2))
+    s_list.add_in_tail(Node(128))
+    s_list.add_in_tail(Node(128))
+    s_list.add_in_tail(Node(9))
+    s_list.add_in_tail(Node(9))
+    s_list.add_in_tail(Node(1))
+    s_list.add_in_tail(Node(9))
+    s_list.add_in_tail(Node(1))
+    s_list.add_in_tail(Node(9))
+
+    s_list.clean()
+
+    assert s_list.len() == 0
+
+    assert s_list.head is None
+    assert s_list.tail is None
+
+
+def mass_search_test():
+    s_list = LinkedList()
+    s_list.add_in_tail(Node(1))
+    s_list.add_in_tail(Node(1))
+    s_list.add_in_tail(Node(1))
+    s_list.add_in_tail(Node(2))
+    s_list.add_in_tail(Node(2))
+    s_list.add_in_tail(Node(128))
+    s_list.add_in_tail(Node(128))
+    s_list.add_in_tail(Node(9))
+    s_list.add_in_tail(Node(9))
+    s_list.add_in_tail(Node(1))
+    s_list.add_in_tail(Node(9))
+    s_list.add_in_tail(Node(1))
+    s_list.add_in_tail(Node(9))
+
+    assert len(s_list.find_all(9)) == 4
+    assert len(s_list.find_all(1)) == 5
+    assert len(s_list.find_all(128)) == 2
+
+
+def size_test():
+    s_list = LinkedList()
+    s_list.add_in_tail(Node(1))
+    assert s_list.len() == 1
+    s_list.add_in_tail(Node(1))
+    assert s_list.len() == 2
+    s_list.add_in_tail(Node(1))
+    s_list.add_in_tail(Node(2))
+    s_list.add_in_tail(Node(2))
+    assert s_list.len() == 5
+    s_list.add_in_tail(Node(128))
+    s_list.add_in_tail(Node(128))
+    s_list.add_in_tail(Node(9))
+    s_list.add_in_tail(Node(9))
+    s_list.add_in_tail(Node(1))
+    assert s_list.len() == 10
+    s_list.add_in_tail(Node(9))
+    s_list.add_in_tail(Node(1))
+    assert s_list.len() == 12
+    s_list.add_in_tail(Node(9))
+    assert s_list.len() == 13
+
+
+def add_in_pos_test():
+    s_list = LinkedList()
+    s_list.add_in_tail(Node(1))
+    s_list.add_in_tail(Node(1))
+    s_list.add_in_tail(Node(1))
+    s_list.add_in_tail(Node(2))
+    s_list.add_in_tail(Node(2))
+    s_list.add_in_tail(Node(128))
+    s_list.add_in_tail(Node(128))
+    s_list.add_in_tail(Node(9))
+    s_list.add_in_tail(Node(9))
+    s_list.add_in_tail(Node(1))
+    s_list.add_in_tail(Node(9))
+    s_list.add_in_tail(Node(1))
+    s_list.add_in_tail(Node(9))
+
+    s_list_test = LinkedList()
+    s_list_test.add_in_tail(Node(-9))
+    s_list_test.add_in_tail(Node(-10))
+    s_list_test.add_in_tail(Node(-11))
+    s_list_test.add_in_tail(Node(-2))
+    s_list_test.add_in_tail(Node(1))
+    s_list_test.add_in_tail(Node(1))
+    s_list_test.add_in_tail(Node(1))
+    s_list_test.add_in_tail(Node(2))
+    s_list_test.add_in_tail(Node(-1))
+    s_list_test.add_in_tail(Node(-3))
+    s_list_test.add_in_tail(Node(2))
+    s_list_test.add_in_tail(Node(128))
+    s_list_test.add_in_tail(Node(128))
+    s_list_test.add_in_tail(Node(9))
+    s_list_test.add_in_tail(Node(9))
+    s_list_test.add_in_tail(Node(1))
+    s_list_test.add_in_tail(Node(9))
+    s_list_test.add_in_tail(Node(1))
+    s_list_test.add_in_tail(Node(9))
+    s_list_test.add_in_tail(Node(-111))
+
+    s_list.insert(None, Node(-11))
+    s_list.insert(None, Node(-10))
+    s_list.insert(None, Node(-9))
+    s_list.insert(s_list.head.next.next.next.next.next.next, Node(-1))
+    s_list.insert(s_list.head.next.next, Node(-2))
+    s_list.insert(s_list.head.next.next.next.next.next.next.next.next, Node(-3))
+    s_list.insert(s_list.tail, Node(-111))
+
+    assert s_list.len() == s_list_test.len()
+
+    node = s_list.head
+    node_test = s_list_test.head
+    while node is not None and node_test is not None:
+        assert node.value == node_test.value
+        node = node.next
+        node_test = node_test.next
+
+
+def merge_test():
+    s_list_one = LinkedList()
+    s_list_one.add_in_tail(Node(1))
+    s_list_one.add_in_tail(Node(2))
+    s_list_one.add_in_tail(Node(3))
+    s_list_one.add_in_tail(Node(4))
+    s_list_one.add_in_tail(Node(5))
+    s_list_one.add_in_tail(Node(6))
+    s_list_one.add_in_tail(Node(7))
+    s_list_one.add_in_tail(Node(8))
+    s_list_one.add_in_tail(Node(9))
+    s_list_one.add_in_tail(Node(10))
+    s_list_one.add_in_tail(Node(11))
+    s_list_one.add_in_tail(Node(12))
+    s_list_one.add_in_tail(Node(13))
+
+    s_list_two = LinkedList()
+    s_list_two.add_in_tail(Node(13))
+    s_list_two.add_in_tail(Node(12))
+    s_list_two.add_in_tail(Node(11))
+    s_list_two.add_in_tail(Node(10))
+    s_list_two.add_in_tail(Node(9))
+    s_list_two.add_in_tail(Node(8))
+    s_list_two.add_in_tail(Node(7))
+    s_list_two.add_in_tail(Node(6))
+    s_list_two.add_in_tail(Node(5))
+    s_list_two.add_in_tail(Node(4))
+    s_list_two.add_in_tail(Node(3))
+    s_list_two.add_in_tail(Node(2))
+    s_list_two.add_in_tail(Node(1))
+
+    s_list_merged = merge_list(s_list_two, s_list_one)
+
+    s_list_test = LinkedList()
+
+    s_list_test.add_in_tail(Node(14))
+    s_list_test.add_in_tail(Node(14))
+    s_list_test.add_in_tail(Node(14))
+    s_list_test.add_in_tail(Node(14))
+    s_list_test.add_in_tail(Node(14))
+    s_list_test.add_in_tail(Node(14))
+    s_list_test.add_in_tail(Node(14))
+    s_list_test.add_in_tail(Node(14))
+    s_list_test.add_in_tail(Node(14))
+    s_list_test.add_in_tail(Node(14))
+    s_list_test.add_in_tail(Node(14))
+    s_list_test.add_in_tail(Node(14))
+    s_list_test.add_in_tail(Node(14))
+
+    assert s_list_merged.len() == s_list_test.len()
+
+    node = s_list_merged.head
+    node_test = s_list_test.head
+    while node is not None and node_test is not None:
+        assert node.value == node_test.value
+        node = node.next
+        node_test = node_test.next
+
+
+if __name__ == '__main__':
+    remove_test()
+    mass_remove_test()
+    clear_test()
+    mass_search_test()
+    size_test()
+    add_in_pos_test()
+    merge_test()
