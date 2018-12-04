f5a71dddc507d65b1f053fbcce43477ea2fdcd11
BlackFox <sitkineragon@mail.ru>
2018-12-04 17:22:26 +1000

    feat: add new tests for double linked lists

--- test/linked_list2.py
+++ test/linked_list2.py
@@ -22,6 +22,7 @@ class TestLinkedList2(unittest.TestCase):
         self.assertEqual(self.list.find(1), node_list[0])
         self.assertEqual(self.list.find(2), node_list[3])
         self.assertEqual(self.list.find(9), node_list[5])
+        self.assertIsNone(self.list.find(123))
 
     def test_find_all(self):
         node_list = [
@@ -40,6 +41,7 @@ class TestLinkedList2(unittest.TestCase):
         self.assertEqual(self.list.find_all(2), node_list[3:5])
         self.assertEqual(self.list.find_all(9), node_list[5:7])
         self.assertEqual(self.list.find_all(10), node_list[-1:])
+        self.assertEqual(self.list.find_all(3123), [])
 
     def test_delete(self):
         node_list = [
@@ -130,7 +132,12 @@ class TestLinkedList2(unittest.TestCase):
         self.assertEqual(self.list.head.next.next.next.next.next, node_list[3])
 
     def test_add_in_head(self):
-        node_list = [
+        insert_node = [
+            Node(2),
+            Node(5),
+            Node(7),
+            Node(1),
+            Node(10),
             Node(1),
             Node(1),
             Node(1),
@@ -141,23 +148,14 @@ class TestLinkedList2(unittest.TestCase):
             Node(10)
         ]
 
-        insert_node = [
-            Node(2),
-            Node(5),
-            Node(7),
-            Node(1),
-            Node(10),
-        ]
-        for node in node_list:
-            self.list.add_in_tail(node)
-
         for node in insert_node:
             prev_head = self.list.head
             self.list.add_in_head(node)
             self.assertEqual(self.list.head, node)
             self.assertIsNone(self.list.head.prev)
             self.assertEqual(self.list.head.next, prev_head)
-            self.assertEqual(prev_head.prev, self.list.head)
+            if prev_head is not None:
+                self.assertEqual(prev_head.prev, self.list.head)
 
     def test_clean(self):
         node_list = [
