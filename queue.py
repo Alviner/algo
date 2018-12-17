d340f6bd77d7541141204a5a5a242726bb32bea8
alviner <gr.eyed.black.fox@gmail.com>
2018-12-18 04:03:45 +1000

    refactor: some changes

--- queue.py
+++ queue.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from stack import Stack
-
 
 class Queue:
     def __init__(self):
@@ -24,24 +22,4 @@ class Queue:
             self.enqueue(self.dequeue())
 
 
-class StackQueue:
-    def __init__(self):
-        self.right = Stack()
-        self.left = Stack()
-
-    def enqueue(self, item):  # O(n)
-        self.right.push(item)
-
-    def dequeue(self):  # O(n)
-        if self.left.size() == 0:
-            while self.right.size() > 0:
-                self.left.push(self.right.pop())
-        return self.left.pop()
-
-    def rotate(self, N):
-        for i in range(N):
-            self.enqueue(self.dequeue())
-
-    def size(self):
-        return self.right.size() + self.left.size()
 
